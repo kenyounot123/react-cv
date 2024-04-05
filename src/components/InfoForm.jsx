b3f650cdf7cbfb35bb819c9164c29ebe9f85afbe
Ken Lu <kenlu519@gmail.com>
2024-04-04 21:39:22 -0400

    update responsive layout and design for larger screens

--- src/components/InfoForm.jsx
+++ src/components/InfoForm.jsx
@@ -4,56 +4,68 @@ export default function InfoForm({ defaultInfo, handleChange }) {
     <div className="form-info-container container">
       <h3 className="form-info-title"> General Information </h3>
       <form className="form-info-section">
-        <label className="input-field-labels" htmlFor="firstName">
-          First Name
-        </label>
-        <input
-          value={defaultInfo.generalInfo.firstName}
-          onChange={(e) =>
-            handleChange("generalInfo", "firstName", e.target.value)
-          }
-          id="firstName"
-          className="input-field"
-          type="text"
-        />
+        <div className="first-and-last-name">
+          <div className="first-name-field">
+            <label className="input-field-labels" htmlFor="firstName">
+              First Name
+            </label>
+            <input
+              value={defaultInfo.generalInfo.firstName}
+              onChange={(e) =>
+                handleChange("generalInfo", "firstName", e.target.value)
+              }
+              id="firstName"
+              className="input-field"
+              type="text"
+            />
+          </div>
+          <div className="last-name-field">
+            <label className="input-field-labels" htmlFor="lastName">
+              Last Name
+            </label>
+            <input
+              required
+              value={defaultInfo.generalInfo.lastName}
+              onChange={(e) =>
+                handleChange("generalInfo", "lastName", e.target.value)
+              }
+              id="lastName"
+              className="input-field"
+              type="text"
+            />
+          </div>
+        </div>
 
-        <label className="input-field-labels" htmlFor="lastName">
-          Last Name
-        </label>
-        <input
-          required
-          value={defaultInfo.generalInfo.lastName}
-          onChange={(e) =>
-            handleChange("generalInfo", "lastName", e.target.value)
-          }
-          id="lastName"
-          className="input-field"
-          type="text"
-        />
-
-        <label className="input-field-labels" htmlFor="phoneNumber">
-          Phone Number
-        </label>
-        <input
-          value={defaultInfo.generalInfo.phoneNumber}
-          onChange={(e) =>
-            handleChange("generalInfo", "phoneNumber", e.target.value)
-          }
-          id="phoneNumber"
-          className="input-field"
-          type="text"
-        />
-
-        <label className="input-field-labels" htmlFor="email">
-          Email
-        </label>
-        <input
-          value={defaultInfo.generalInfo.email}
-          onChange={(e) => handleChange("generalInfo", "email", e.target.value)}
-          id="email"
-          className="input-field"
-          type="email"
-        />
+        <div className="phone-and-email">
+          <div className="phone-field">
+            <label className="input-field-labels" htmlFor="phoneNumber">
+              Phone Number
+            </label>
+            <input
+              value={defaultInfo.generalInfo.phoneNumber}
+              onChange={(e) =>
+                handleChange("generalInfo", "phoneNumber", e.target.value)
+              }
+              id="phoneNumber"
+              className="input-field"
+              type="text"
+            />
+          </div>
+          <div className="email-field">
+            <label className="input-field-labels" htmlFor="email">
+              Email
+            </label>
+            <input
+              value={defaultInfo.generalInfo.email}
+              onChange={(e) =>
+                handleChange("generalInfo", "email", e.target.value)
+              }
+              id="email"
+              className="input-field"
+              type="email"
+            />
+          </div>
+        </div>
       </form>
     </div>
   );
